b4642b482542dac7b63af37a77a748402474e19c
godlycat <ufufhby74@gmail.com>
2021-03-07 15:38:56 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let len =str.length
+  for(let i = 0; i<=len; i++){
+    bracketsConfig.map((item)=>{
+      str=str.replace(item[0]+item[1],"")
+    })
+  }
+  if(str) return false
+    else return true
 }
